b8c2c18efc1569a0fdb02120c1441e594c547c9d
5milon <5milon5ali@gmail.com>
2024-09-05 19:14:31 +0600

    All Codes

---
+++ 2_concat_coercion/script.js
@@ -0,0 +1,27 @@
+// const str = "hello " + " World";
+// console.log(str);
+
+// let sum = "5 "+ 4;
+// console.log(sum);
+
+
+/**
+ * Tricky Interview Question
+ */
+
+// console.log(10 + '20');
+
+// console.log(9 - "7");
+
+// console.log("java " + "Script");
+
+// console.log(' ' + ' ');
+let sum = " " + 0;
+console.log(sum);
+console.log(typeof sum);
+
+// console.log("vinod" - "thapa");
+// console.log(true + true);
+// console.log(true + false);
+// console.log(false + true);
+// console.log(false - true);
---
+++ 3_expressions/script.js
@@ -0,0 +1,8 @@
+var age = 19;
+var result = age >=18 ? "Yes": "No";
+console.log(result);
+var score = 99;
+var mane = score>=60 ? "pass": "fail";
+console.log(mane);
+console.log(2<12<5);
+console.log("20" + 10 + 10);
